46c3a6373dfc049f4f9a8def364adb6f08f73724
Флорко Андрей <aflorko@logstream.ru>
2025-03-09 15:28:42 +0300

    fix: changes migrated from special branch gh-pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,8 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
+        <h1>Андрей Флорко</h1>
+        <p className="App-goal">
+          <strong>Цель прохождения курса:</strong> систематизация знаний React, которые понадобятся в коммерческой
+          разработке для нужд компании
+        </p>
+        <p className="App-tech">
+          <strong>Технологии:</strong> Full Stack .NET разработчик (преимущественно Back-End)
+        </p>
+        <p className="App-experience">
+          <strong>Опыт:</strong> 20 лет разработки
         </p>
       </header>
     </div>
