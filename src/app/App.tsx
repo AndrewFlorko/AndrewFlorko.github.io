7079818f090031cee1c67a37fa1d46fca2fb4776
Флорко Андрей <aflorko@logstream.ru>
2025-03-09 10:52:19 +0300

    minor: lint updates + styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,10 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Андрей Флорко</p>
-        <p>Цель прохождения курса: систематизация знаний React, которые понадобятся в коммерческой разработки для нужд компании</p>
-        <p>Технологии: Full Stack .NET разработчик (преимущественно Back-End)</p>
-        <p>20 лет опыта разработки</p>
+        <h1>Андрей Флорко</h1>
+        <p className="App-goal">
+          <strong>Цель прохождения курса:</strong> систематизация знаний React, которые понадобятся в коммерческой
+          разработке для нужд компании
+        </p>
+        <p className="App-tech">
+          <strong>Технологии:</strong> Full Stack .NET разработчик (преимущественно Back-End)
+        </p>
+        <p className="App-experience">
+          <strong>Опыт:</strong> 20 лет разработки
+        </p>
       </header>
     </div>
   );
