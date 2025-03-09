ef2db03aa25751748baa90500ca7df67dde83138
Флорко Андрей <aflorko@logstream.ru>
2025-03-09 09:33:39 +0300

    minor: basic information for main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <p>Андрей Флорко</p>
+        <p>Цель прохождения курса: систематизация знаний React, которые понадобятся в коммерческой разработки для нужд компании</p>
+        <p>Технологии: Full Stack .NET разработчик (преимущественно Back-End)</p>
+        <p>20 лет опыта разработки</p>
       </header>
     </div>
   );
